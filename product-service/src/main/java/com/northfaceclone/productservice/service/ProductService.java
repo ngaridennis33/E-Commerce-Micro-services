41293af8279ff93ea432d894988ee303469344c1
ngaridennis33 <ngaridennis33@gmail.com>
2024-05-22 01:27:43 +0300

    feat: Initialize product Service

--- product-service/src/main/java/com/northfaceclone/productservice/service/ProductService.java
+++ product-service/src/main/java/com/northfaceclone/productservice/service/ProductService.java
@@ -1,2 +1,23 @@
-package com.northfaceclone.productservice.service;public class ProductService {
+package com.northfaceclone.productservice.service;
+
+import com.northfaceclone.productservice.models.Products;
+import com.northfaceclone.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void saveProduct(Products user){
+        productRepository.save(user);
+    }
+
+    public List<Products> findAllProducts(){
+        return productRepository.findAll();
+    }
 }
