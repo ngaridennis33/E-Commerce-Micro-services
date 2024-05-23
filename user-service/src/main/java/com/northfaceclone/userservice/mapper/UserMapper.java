e8b703d8641276dd3b712b17a39c79622ec9d9ff
ngaridennis33 <ngaridennis33@gmail.com>
2024-05-24 02:11:30 +0300

    feat: Map the UserDTO

--- user-service/src/main/java/com/northfaceclone/userservice/mapper/UserMapper.java
+++ user-service/src/main/java/com/northfaceclone/userservice/mapper/UserMapper.java
@@ -1,2 +1,28 @@
-package com.northfaceclone.userservice.mapper;public class UserMapper {
+package com.northfaceclone.userservice.mapper;
+
+import com.northfaceclone.userservice.dto.UserResponseDTO;
+import com.northfaceclone.userservice.models.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserMapper {
+    public User toUser(UserResponseDTO dto){
+        if(dto == null){
+            throw new NullPointerException("The student DTO should NOT be null");
+        }
+        var user = new User();
+        user.setFirst_name(dto.firstname());
+        user.setLast_name(dto.lastname());
+        user.setEmail(dto.email());
+
+        return user;
+    };
+
+    public UserResponseDTO toUserResponseDTO(User user){
+        return new UserResponseDTO(
+                user.getFirst_name(),
+                user.getLast_name(),
+                user.getEmail()
+        );
+    }
 }
