7aebe866d0a89af4850ec28bd53b456f6e8d3ed9
ngaridennis33 <ngaridennis33@gmail.com>
2024-06-02 09:37:33 +0300

    fix: Design the model structure of Business Error codes

--- user-service/src/main/java/com/northfaceclone/userservice/dto/model/BusinessErrorCodes.java
+++ user-service/src/main/java/com/northfaceclone/userservice/dto/model/BusinessErrorCodes.java
@@ -1,2 +1,38 @@
-package com.northfaceclone.userservice.dto.model;public enum BusinessErrorCodes {
+package com.northfaceclone.userservice.dto.model;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+public enum BusinessErrorCodes {
+
+    NO_CODE(0, NOT_IMPLEMENTED, "No Code"),
+
+    ACCOUNT_LOCKED(302, FORBIDDEN, "User Account is Locked"),
+
+    NEW_PASSWORD_DOES_NOT_MATCH(400, BAD_REQUEST, "New password does not match"),
+
+    INCORRECT_CURRENT_PASSWORD(400, BAD_REQUEST, "Current password is incorrect"),
+
+    ACCOUNT_DISABLED(403, FORBIDDEN, "User Account is Disabled"),
+
+    BAD_CREDENTIALS(401, UNAUTHORIZED, "Email and/or password is incorrect");
+
+    @Getter
+    private final int code;
+    @Getter
+    private final String description;
+    @Getter
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.description = description;
+
+   }
+
 }
+
+
