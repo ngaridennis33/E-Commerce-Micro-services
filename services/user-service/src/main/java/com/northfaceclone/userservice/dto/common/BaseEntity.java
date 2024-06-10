f12379ad3c5cc21f5a6d9c7b0af2515aa3fff1f0
ngaridennis33 <ngaridennis33@gmail.com>
2024-06-10 10:01:49 +0300

    Updated the config server

--- services/user-service/src/main/java/com/northfaceclone/userservice/dto/common/BaseEntity.java
+++ services/user-service/src/main/java/com/northfaceclone/userservice/dto/common/BaseEntity.java
@@ -1,13 +1,12 @@
 package com.northfaceclone.userservice.dto.common;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
