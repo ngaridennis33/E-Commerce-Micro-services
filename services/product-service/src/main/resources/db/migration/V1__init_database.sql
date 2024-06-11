7274218a8d1ec6bfcec8bd1aa823679e85961264
ngaridennis33 <ngaridennis33@gmail.com>
2024-06-11 06:34:14 +0300

    chore: Add Database Migrations

--- services/product-service/src/main/resources/db/migration/V1__init_database.sql
+++ services/product-service/src/main/resources/db/migration/V1__init_database.sql
@@ -1,23 +1,29 @@
-create table if not exists category
-(
-    id          integer not null
-        primary key,
-    description varchar(255),
-    name        varchar(255)
+CREATE TABLE IF NOT EXISTS category (
+    id          BIGINT NOT NULL PRIMARY KEY,
+    description VARCHAR(255),
+    name        VARCHAR(255)
 );
 
-create table if not exists product
-(
-    id                 integer          not null
-        primary key,
-    available_quantity double precision not null,
-    description        varchar(255),
-    name               varchar(255),
-    price              numeric(38, 2),
-    category_id        integer
-        constraint fk1mtsbur82frn64de7balymq9s
-            references category
+CREATE TABLE IF NOT EXISTS product (
+    id                 BIGINT NOT NULL PRIMARY KEY,
+    available_quantity DOUBLE PRECISION NOT NULL,
+    description        VARCHAR(255),
+    name               VARCHAR(255),
+    price              NUMERIC(38, 2),
+    category_id        BIGINT REFERENCES category
 );
 
-create sequence if not exists category_seq increment by 50;
-create sequence if not exists product_seq increment by 50;
+CREATE SEQUENCE IF NOT EXISTS category_seq INCREMENT BY 50;
+CREATE SEQUENCE IF NOT EXISTS product_seq INCREMENT BY 50;
+
+ALTER TABLE category
+    ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE category
+    ADD COLUMN last_modified_date TIMESTAMP;
+
+ALTER TABLE product
+    ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE product
+    ADD COLUMN last_modified_date TIMESTAMP;
