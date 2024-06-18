74ae44cbaffe731aab0a56cdf42c529fe509fb55
ngaridennis33 <ngaridennis33@gmail.com>
2024-06-19 02:20:10 +0300

    chore: Update the project architecture

--- services/auth-service/src/main/java/com/northfaceclone/authservice/security/JwtService.java
+++ services/auth-service/src/main/java/com/northfaceclone/authservice/security/JwtService.java
@@ -7,7 +7,6 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
